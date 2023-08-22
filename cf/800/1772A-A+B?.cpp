85ef90646389039205ad624c63e91ade6f8a960b
Pranshav Patel <96743783+pranshavpatel@users.noreply.github.com>
2023-08-22 21:36:16 +0530

    Create 1772A-A+B?.cpp

---
+++ cf/800/1772A-A+B?.cpp
@@ -0,0 +1,24 @@
+// https://codeforces.com/problemset/problem/1772/A
+
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(true); cin.tie(NULL); cout.tie(NULL)
+#define PI 3.141592653589793238462643383279
+
+using namespace std;
+
+void solve(){
+    string s;
+    cin>>s;
+    cout<<s[0] - '0' + s[2] - '0'<<endl;
+}
+
+int main(){
+    fast;
+    int t;
+    cin>>t;
+    // t=1;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
